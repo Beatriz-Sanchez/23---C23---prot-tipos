2fc46fa45686dcb172d531ee609ed9481a9a29af
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2021-09-22 11:02:07 -0300

    Update Caixa.js

--- Caixa.js
+++ Caixa.js
@@ -2,7 +2,7 @@ class Caixa {
   constructor(x,y,width,height){
     var options = {
       restitution: 0.8
-    }
+    };
     this.body = Bodies.rectangle(x,y,width,height,options);
     World.add(world,this.body);
     this.width = width;
