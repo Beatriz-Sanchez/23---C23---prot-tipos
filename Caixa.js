866a9dae573f11992205346a223d973cdbc338e1
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2021-09-22 10:55:26 -0300

    Update Caixa.js

--- Caixa.js
+++ Caixa.js
@@ -1,9 +1,9 @@
 class Caixa {
-  constructor(){
-    this.body = Bodies.rectangle(200,100,50,50);
+  constructor(x,y,width,height){
+    this.body = Bodies.rectangle(x,y,width,height);
     World.add(world,this.body);
-    this.width = 50;
-    this.height = 50;
+    this.width = width;
+    this.height = height;
   }
   display(){
     var pos = this.body.position;
